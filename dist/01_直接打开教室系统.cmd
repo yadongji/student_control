860aa1838da5e1157cef24f43b2fe9bbdd7cac50
161616 <514451577@qq.com>
2026-09-10 20:38:40 +0800

    其他入口

---
+++ dist/01_直接打开教室系统.cmd
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+setlocal
+if not exist "%~dp0学生综合管理系统.exe" (
+ echo 请把本文件放到学生综合管理系统.exe所在的文件夹，再双击运行。
+ pause
+ exit /b 1
+)
+pushd "%~dp0"
+if errorlevel 1 exit /b 1
+start "" "%~dp0学生综合管理系统.exe" --classroom
+popd
+endlocal
